aa7ed77516a5078e20b88c39c4a5e561f4bcb8c7
Marcelo <marcelocesarti@gmail.com>
2024-04-22 20:33:58 -0300

    primeiro commit do projeto

---
+++ src/main/java/Projeto/Ecommerce/de/Livros/Ecommerce/de/Livros/ECommerceDeLivrosApplication.java
@@ -0,0 +1,13 @@
+package Projeto.Ecommerce.de.Livros.Ecommerce.de.Livros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceDeLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceDeLivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Projeto/Ecommerce/de/Livros/Ecommerce/de/Livros/ECommerceDeLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package Projeto.Ecommerce.de.Livros.Ecommerce.de.Livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceDeLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
